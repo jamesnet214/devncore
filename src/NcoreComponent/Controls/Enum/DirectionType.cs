a54167ce30409eb40c12766cae74f1fa12cef306
kevin.kim <rlagurdls456@outlook.com>
2021-11-24 20:41:41 +0900

    Needle 추가

---
+++ src/NcoreComponent/Controls/Enum/DirectionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NcoreComponent.Controls
+{
+    public enum DirectionType
+    {
+        N = 0,
+        NE = 45,
+        E = 90,
+        SE = 135,
+        S = 180,
+        SW = 225,
+        W = 270,
+        NW = 315,
+    }
+}
