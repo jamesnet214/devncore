e4c8525c84209f5435db748acbffa454fe4539fb
nuhabo <nuhabo03@gmail.com>
2021-11-25 21:58:21 +0900

    Update 각도정보추가

--- src/NcoreComponent/Controls/Compass.xaml.cs
+++ src/NcoreComponent/Controls/Compass.xaml.cs
@@ -32,12 +32,15 @@ namespace NcoreComponent.Controls
         double cy;
         // 가장 바깥
         double label_R;
-        double gauge_R;
-        double large_R;
-        double small_R;
+
+        double bigCircle_R;
+        double outCircle_R;
+        double tiny_R;
+        double inCircle_R;
         const int degreeOffset = 270;
         const int primaryTick = 45;
         const int SecondTick = 3;
+
         
         #endregion
 
@@ -49,45 +52,13 @@ namespace NcoreComponent.Controls
 
         private void SetScale()
         {
-            //cx = canvas.Width / 2;
-            //cy = canvas.Height / 2;
-            //label_R = cx;
-            //gauge_R = label_R * 0.9;
-            //large_R = gauge_R * 0.8;
-
-            //// 360번 for 문
-            //int gaugeDegree = 360;
-
-            //// Thickness 테두리
-            //for (int i = 0; i < gaugeDegree; i++)
-            //{
-            //    double cos = (double)Math.Cos(DegreesToRadians(i + degreeOffset));
-            //    double sin = (double)Math.Sin(DegreesToRadians(i + degreeOffset));
-
-            //    double x1 = cx + gauge_R * cos;
-            //    double y1 = cy + gauge_R * sin;
-
-            //    double x2 = cx + large_R * cos;
-            //    double y2 = cx + large_R * sin;
-
-            //    Line line = new Line();
-            //    line.SetResourceReference(Shape.StrokeProperty, "Compas.Needle");
-            //    line.StrokeThickness = 1;
-
-            //    line.X1 = x1;
-            //    line.X2 = x2;
-            //    line.Y1 = y1;
-            //    line.Y2 = y2;
-            //    canvas.Children.Add(line);
-            //}
-
             cx = canvas.Width / 2;
             cy = canvas.Height / 2;
             label_R = cx;
-            gauge_R = label_R * 0.85;
-            large_R = gauge_R * 0.85;
-            small_R = gauge_R * 0.75;
-
+            bigCircle_R = label_R * 1;
+            outCircle_R = label_R * 0.85;
+            tiny_R = outCircle_R * 0.85;
+            inCircle_R = outCircle_R * 0.75;
 
             int gaugeDegree = 360;
             for (int i = 0; i < gaugeDegree; i++)
@@ -95,11 +66,11 @@ namespace NcoreComponent.Controls
                 double cos = (double)Math.Cos(DegreesToRadians(i + degreeOffset));
                 double sin = (double)Math.Sin(DegreesToRadians(i + degreeOffset));
 
-                double x1 = cx + gauge_R * cos;
-                double y1 = cy + gauge_R * sin;
+                double x1 = cx + outCircle_R * cos;
+                double y1 = cy + outCircle_R * sin;
 
-                double x2 = cx + small_R * cos;
-                double y2 = cy + small_R * sin;
+                double x2 = cx + inCircle_R * cos;
+                double y2 = cy + inCircle_R * sin;
 
                 Line line = new Line();
                 line.SetResourceReference(Shape.StrokeProperty, "Compas.Back");
@@ -113,28 +84,45 @@ namespace NcoreComponent.Controls
                 canvas.Children.Add(line);
             }
 
-
             for (int i = 0; i < 360; i += SecondTick)
             {
                 double cos = (double)Math.Cos(DegreesToRadians(i + degreeOffset));
                 double sin = (double)Math.Sin(DegreesToRadians(i + degreeOffset));
 
-                double x1 = cx + small_R * cos;
-                double y1 = cy + small_R * sin;
+                double x1 = cx + inCircle_R * cos;
+                double y1 = cy + inCircle_R * sin;
                 double x2 = 0;
                 double y2 = 0;
 
                 if (i % primaryTick == 0)
                 {
                     // 45도씩 오는거
-                    x2 = cx + gauge_R * cos;
-                    y2 = cy + gauge_R * sin;
+                    x2 = cx + outCircle_R * cos;
+                    y2 = cy + outCircle_R * sin;
+
+                    double x3 = cx + bigCircle_R * cos;
+                    double y3 = cy + bigCircle_R * sin;
+
+                    Label label = new Label();
+                    label.Content = $"{i} 도";
+                    label.Foreground = Brushes.Blue;
+
+                    label.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+                    Size s = label.DesiredSize;
+                    
+
+
+                    Canvas.SetLeft(label, x3 - (s.Width / 2));
+                    Canvas.SetTop(label, y3 - (s.Height / 2));
+                    canvas.Children.Add(label);
+
                 }
                 else
                 {
                     // 3도씩오는거
-                    x2 = cx + large_R * cos;
-                    y2 = cy + large_R * sin;
+                    x2 = cx + tiny_R * cos;
+                    y2 = cy + tiny_R * sin;
                 }
 
                 //double x1 = cx + gauge_R * cos;
