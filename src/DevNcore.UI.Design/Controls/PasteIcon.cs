8e5c2df32be85f42a9b53f078cea08f1fbe6d602
james.lee <james.lee@devncore.org>
2022-03-18 16:52:59 +0900

    Added Paste, EyeDropper Icon

---
+++ src/DevNcore.UI.Design/Controls/PasteIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevNcore.UI.Design.Controls.Primitives;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class PasteIcon : Icon
+    {
+        static PasteIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PasteIcon), new FrameworkPropertyMetadata(typeof(PasteIcon)));
+        }
+    }
+}
