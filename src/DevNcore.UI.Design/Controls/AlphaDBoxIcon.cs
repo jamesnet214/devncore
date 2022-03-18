46cc1a6bffbd61105bbb5cbbc2f298654c9e6bf4
james.lee <james.lee@devncore.org>
2022-03-18 19:41:53 +0900

    Update Design

---
+++ src/DevNcore.UI.Design/Controls/AlphaDBoxIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevNcore.UI.Design.Controls.Primitives;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class AlphaDBoxIcon : Icon
+    {
+        static AlphaDBoxIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AlphaDBoxIcon), new FrameworkPropertyMetadata(typeof(AlphaDBoxIcon)));
+        }
+    }
+}
