f42346ae52aec0afb77ae3b1d38d38d0ad2b30d0
james.lee <james.lee@devncore.org>
2022-03-23 02:33:31 +0900

    Update nuget icon

---
+++ src/DevNcore.UI.Design/Controls/Icons/LogoutVariantIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevNcore.UI.Design.Controls.Primitives;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class LogoutVariantIcon : Icon
+    {
+        static LogoutVariantIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LogoutVariantIcon), new FrameworkPropertyMetadata(typeof(LogoutVariantIcon)));
+        }
+    }
+}
---
+++ src/DevNcore.UI.Design/Controls/Icons/ReactIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevNcore.UI.Design.Controls.Primitives;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class ReactIcon : Icon
+    {
+        static ReactIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ReactIcon), new FrameworkPropertyMetadata(typeof(ReactIcon)));
+        }
+    }
+}
