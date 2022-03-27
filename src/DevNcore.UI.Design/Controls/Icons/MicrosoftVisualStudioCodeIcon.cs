80441c6f5c2d67a5726d95129340585f5b546bc8
james.lee <james.lee@devncore.org>
2022-03-27 16:15:28 +0900

    Added MicrosoftVisualStudioCodeIcon

---
+++ src/DevNcore.UI.Design/Controls/Icons/MicrosoftVisualStudioCodeIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevNcore.UI.Design.Controls.Primitives;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class MicrosoftVisualStudioCodeIcon : Icon
+    {
+        static MicrosoftVisualStudioCodeIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MicrosoftVisualStudioCodeIcon), new FrameworkPropertyMetadata(typeof(MicrosoftVisualStudioCodeIcon)));
+        }
+    }
+}
