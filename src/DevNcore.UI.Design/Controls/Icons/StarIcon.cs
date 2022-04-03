55871fd714f14b66484da95a216caf207086907b
unknown <james.lee@devncore.org>
2022-04-03 18:28:38 +0900

    Added StarIcon.cs

---
+++ src/DevNcore.UI.Design/Controls/Icons/StarIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using DevNcore.UI.Design.Controls.Primitives;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class StarIcon : Icon
+    {
+        static StarIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StarIcon), new FrameworkPropertyMetadata(typeof(StarIcon)));
+        }
+    }
+}
